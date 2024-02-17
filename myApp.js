ab268d397dc13ae4b7a847d960cf21b0223bc6af
Peter Kwok <peter.kwok94@gmail.com>
2024-02-17 18:50:43 -0500

    Initial commit

--- myApp.js
+++ myApp.js
@@ -1,39 +1,41 @@
-let express = require('express');
+let express = require("express");
 let app = express();
+require("dotenv").config()
 
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.path} - ${req.ip}`);
+    next();
+});
 
+app.use("/public", express.static(__dirname + "/public"));
 
+absolutePath = __dirname + "/views/index.html";
 
+app.get("/", (req, res) => {
+    res.sendFile(absolutePath);
+});
 
+app.get("/json", (req, res) => {
+    let response = "Hello json";
+    if (process.env.MESSAGE_STYLE === "uppercase") {
+        response = response.toUpperCase();
+    }
+    res.json({ message: response });
+});
 
+app.get(
+    "/now",
+    (req, res, next) => {
+        req.time = new Date().toString();
+        next();
+    },
+    (req, res) => {
+        res.json({ time: req.time });
+    },
+);
 
+app.get("/:word/echo", (req, res) => {
+    res.json({ echo: req.params.word })
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
